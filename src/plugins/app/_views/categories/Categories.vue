f208a7cb0d0bdf7a9c4f9656f3269c87ce82560e
Peter Knaus <pknaus3@gmail.com>
2021-01-24 18:53:37 +0100

    Categories fix

--- src/plugins/app/_views/categories/Categories.vue
+++ src/plugins/app/_views/categories/Categories.vue
@@ -2,21 +2,22 @@
 <!-- ToDo: Pozriet vue-layout package -->
 
   <base-layout screen-title = "Kategórie" go-back-link="/">
+
     <ion-grid style="height: 100%">
       <div style="height: 60%;" v-if="categories != null">
-        <router-link v-for="category in categories" :key="category.id" :to="{ path: '/categories/' + category.id + '/cards' }" class="a-category">
+        <router-link v-for="category in notDoneCategories" :key="category.id" :to="{ path: '/categories/' + category.id + '/cards' }" class="a-category">
             <ion-row :style="{'background-color':  category.color }" class="ion-justify-content-center  ion-align-items-center row-category">
                 {{ category.name }}
             </ion-row>
         </router-link>
       </div>
       <div style="height: 40%; padding-top: 25%;">
-        <router-link to="/categories/tutorial" class="a-category">
+        <router-link :to="{name: 'Categories'}" class="a-category">
             <ion-row fake kfja :style="{'background-color':  `#41DC63` }"  class="ion-justify-content-center  ion-align-items-center row-category">
                 Tutorial
             </ion-row>
         </router-link>
-        <router-link to="/my-feeds" class="a-category">
+        <router-link :to="{name: 'Categories'}" class="a-category">
             <ion-row fake :style="{'background-color':  `#19EE88` }" class="ion-justify-content-center ion-align-items-center row-category">
                 Môj Feed
             </ion-row>
@@ -27,11 +28,9 @@
 </template>
 
 <script>
-// ToDo: IonGrid a row includnut globalne (v main.js)
+
 // ToDo: ; do prdele
 // ToDo: ODPORUCANIE: w@tip/no-semis
-// ToDo: Spravit si funkcie do $storu, napr. get cardsByCategory()
-
 
   export default {
     data() {
@@ -39,15 +38,29 @@
         categories: this.$store.getters.getCategories
       }
     },
-    async mounted() {
-      // let doneCategories = [];
-      // await Storage.get({ key: 'isCategoryDone' }).then(resp => { if (resp.value != null) { doneCategories = resp.value.split(','); }});
-      // doneCategories = doneCategories.map(el=>parseInt(el));
+    computed: {
+      notDoneCategories() {
+        let categories = [];
+        let doneCategories = this.$store.getters.getDoneCategories
 
-      if (this.categories[0] == null) {
-        this.$router.push({name: 'Nextdeck'})
+        for (let i = 0; i <= this.categories.length - 1; i++) {
+          if (!doneCategories.includes(this.categories[i].id)) {
+            categories.push(this.categories[i])
+          }
+        }
+
+        if (categories[0] == null) {
+          this.nextDeck()
+        }
+
+        return categories
       }
     },
+    methods: {
+      nextDeck() {
+        this.$router.push({name: 'Nextdeck'})
+      }
+    }
   };
 </script>
 
