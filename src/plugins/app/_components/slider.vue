831aa8347cdb276a9c5f89fea54d69a7a6b44474
Peter Knaus <pknaus3@gmail.com>
2021-01-24 02:21:03 +0100

    Slider with store v0.1

--- src/plugins/app/_components/slider.vue
+++ src/plugins/app/_components/slider.vue
@@ -1,16 +1,34 @@
 <template>
-  <!-- Pozriet si dokumentaciu a mozno pouzit nieco ine ako slider -->
-  <!-- Spravit to bez effektov, iba klikanie na button, uplne bez slideru!!! -->
+  <div v-if="this.cards[0] != null">
     <div v-for="card of renderedCard" :key="card" :style="{'background-color':  card.category.color }">
       {{card.id}}<br/>
+      {{ card.activity }}<br/>
       {{card.description}}<br/>
+      {{ currentCard+1 }} / {{ cards.length }}<br/>
       {{card.hashtag}}<br/>
+
+      <div v-if="cards[currentCard+1] != null">
+        <ion-button @click="nextCard(0)">Decline</ion-button>
+        <ion-button v-if="cards[currentCard+1] != null" @click="nextCard(1)">Accept</ion-button>
+      </div>
+
+      <div v-else>
+        <ion-button @click="nextCategory(0)">Decline</ion-button>
+        <ion-button @click="nextCategory(1)">Accept</ion-button>
+      </div>
+
     </div>
+  </div>
 </template>
 
 <script>
+import {IonButton} from "@ionic/vue";
+
 
 export default {
+  components: {
+    IonButton
+  },
   data() {
     return {
       cards: this.$store.getters.getCardsByCategory(this.categoryId),
@@ -26,6 +44,26 @@ export default {
       return [this.cards[this.currentCard]]
     }
   },
+  methods: {
+    nextCard(isAccepted) {
+      if (isAccepted == 1) {
+        this.$store.dispatch('addAcceptedCard', this.cards[this.currentCard])
+      } else {
+        this.$store.dispatch('addDeclinedCard', this.cards[this.currentCard])
+      }
+
+      this.currentCard++
+    },
+    nextCategory(isAccepted) {
+      if (isAccepted == 1) {
+        this.$store.dispatch('addAcceptedCard', this.cards[this.currentCard])
+      } else {
+        this.$store.dispatch('addDeclinedCard', this.cards[this.currentCard])
+      }
+      this.$store.dispatch('addDoneCategory', this.categoryId)
+      this.$router.push({name: 'Categories'})
+    }
+  }
 }
 
 // 0 = Fajka
--- src/store.js
+++ src/store.js
@@ -3,15 +3,36 @@ import Vuex from 'vuex'
 export default new Vuex.Store({
     state: {
         'campaignData': [],
+        'acceptedCards': [],
+        'declinedCards': [],
+        'doneCategories': []
     },
     actions: {
         setCampaignData({ commit }, value) {
             commit('SET_CAMPAIGN_DATA', value)
+        },
+        addAcceptedCard({ commit }, value) {
+            commit('PUSH_ACCEPTED_CARD', value)
+        },
+        addDeclinedCard({ commit }, value) {
+            commit('PUSH_DECLINED_CARD', value)
+        },
+        addDoneCategory({ commit }, value) {
+            commit('PUSH_DONE_CATEGORY', value)
         }
     },
     mutations: {
         SET_CAMPAIGN_DATA(state, value) {
             state.campaignData = value
+        },
+        PUSH_ACCEPTED_CARD(state, value) {
+            state.acceptedCards.push(value)
+        },
+        PUSH_DECLINED_CARD(state, value) {
+            state.declinedCards.push(value)
+        },
+        PUSH_DONE_CATEGORY(state, value) {
+            state.doneCategories.push(value)
         }
     },
     getters: {
